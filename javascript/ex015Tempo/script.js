27f132c0d30423296b3cfc2b0fa40669e4e582fa
Victor Boy <victor2005boy@gmail.com>
2025-03-12 11:55:33 -0300

    .

---
+++ javascript/ex015Tempo/script.js
@@ -0,0 +1,7 @@
+var agora1 = new Date()
+var hora1 = agora.getHours()
+var tempo=window.document.getElementById('tempo')
+var horas = window.document.getElementById('horas')
+let imagem = window.document.getElementById('imagem')
+
+imagem.src = ""
