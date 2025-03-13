14b10012e29644e896878fd60f076619e8c38743
Victor Boy <victor2005boy@gmail.com>
2025-03-12 21:51:34 -0300

    q
    asdsadasd

--- javascript/ex015Tempo/script.js
+++ javascript/ex015Tempo/script.js
@@ -1,7 +1,30 @@
-var agora1 = new Date()
-var hora1 = agora.getHours()
-var tempo=window.document.getElementById('tempo')
-var horas = window.document.getElementById('horas')
-let imagem = window.document.getElementById('imagem')
+var agora1 = new Date();
+var hora1 = agora.getHours();
+var tempo=window.document.getElementById('tempo');
+var horas = window.document.getElementById('horas');
+let img = window.document.getElementById('imagem');
 
-imagem.src = ""
+
+if(hora1 >= 0 && hora1<=5 ){
+
+tempo.innerText= `boa madrugada`;
+horas.innerText=hora1;
+img.scr='madrugada.jpg';    
+}
+else if(hora<12){
+    tempo.innerText= `bom dia`;
+    horas.innerText=hora1;
+    img.scr='manha.jpg'; 
+
+}
+else if(hora<18){
+    tempo.innerText= `boa noite`;
+    horas.innerText=hora1;
+    img.scr='tarde.jpg'; 
+}
+else{
+    tempo.innerText= `boa noite`;
+    horas.innerText=hora1;
+    img.scr='noite.jpeg'; 
+
+}
